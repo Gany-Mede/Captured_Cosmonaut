f799a6169fee3ef84b48f4f93f445011265fc2bd
Sevastopol06 <vbsevastopol@gmail.com>
2020-10-09 14:04:56 -0400

    loop on navigation

--- src/com/cosmogame/SpaceShip.java
+++ src/com/cosmogame/SpaceShip.java
@@ -15,24 +15,28 @@ class SpaceShip{
     String currentRoom;
     //empty inventory
     List<String> inventory = new ArrayList<>();
-
+    String input;
 
     public void start() {
         //Start at the bridge
         currentRoom = "bridge";
-        System.out.println("Welcome to the game ");
+        System.out.println("Welcome to the game \n");
         printInventory();
         System.out.println("Your are now in this room: " + currentRoom);
         printItems(bridgeItems);
-        prompt();
-        evaluateInput();
-        whereAmI();
-        prompt();
-
+       askAction();
+    }
+    public void askAction(){
+        do{
+            whereAmI();
+            prompt();
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+            evaluateInput();
+        }
+        while(!input.equals("quit"));
     }
     public void evaluateInput(){
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
         String[] inputArray = input.split(" ");
         if (inputArray.length < 2) {
             System.out.println("Wrong command");
@@ -45,23 +49,32 @@ class SpaceShip{
                 System.out.println("You typed in the wrong command");
             }
         }
-        System.out.println("the current room now is: " + currentRoom);
     }
 
     public void whereAmI(){
-        switch(currentRoom){
-            case "bridge":
-                System.out.println(bridgeDescription);
-                printItems(bridgeItems);
-                break;
-            case "gym":
-                System.out.println(gymDescription);
-                printItems(gymItems);
-                break;
-            case "kitchen":
-                System.out.println(kitchenDescription);
-                printItems(kitchenItems);
-                break;
+        try {
+            switch (currentRoom) {
+                case "bridge":
+                    System.out.println("You are now in " + currentRoom);
+                    System.out.println(bridgeDescription);
+                    printItems(bridgeItems);
+                    break;
+                case "gym":
+                    System.out.println("You are now in " + currentRoom);
+                    System.out.println(gymDescription);
+                    printItems(gymItems);
+                    break;
+                case "kitchen":
+                    System.out.println("You are now in " + currentRoom);
+                    System.out.println(kitchenDescription);
+                    printItems(kitchenItems);
+                    break;
+            }
+        }
+        catch(NullPointerException e){
+            System.out.println("Cannot go there");
+            whereAmI();
+            askAction();
         }
     }
     public void printInventory(){
@@ -70,19 +83,34 @@ class SpaceShip{
     public void prompt(){
         System.out.println("You can type 'get (item) to add an item to your inventory'");
         System.out.println("or you can type 'go (direction)' to go to another room");
+        System.out.println("Or type 'quit' to quit the game.");
     }
         public void go(String direction){
-            switch (currentRoom){
-                case "bridge":
-                    currentRoom = bridgeExits.get(direction);
-                    break;
-                case "gym":
-                    currentRoom = gymExits.get(direction);
-                    break;
-                case "kitchen":
-                    currentRoom = kitchenExits.get(direction);
-                    break;
-            }
+        String previousRoom = currentRoom;
+    try{
+          switch (currentRoom){
+        case "bridge":
+            currentRoom = bridgeExits.get(direction);
+            break;
+        case "gym":
+            currentRoom = gymExits.get(direction);
+            break;
+        case "kitchen":
+            currentRoom = kitchenExits.get(direction);
+            break;
+     }
+    }
+    catch (NullPointerException e){
+        System.out.println("Cannot go this way");
+        askAction();
+        whereAmI();
+
+    }
+    if (currentRoom == null){
+        currentRoom = previousRoom;
+        askAction();
+    }
+            System.out.println("the current room is: " + currentRoom);
         }
         public void get(String item){
         inventory.add(item);
